787a299aae5574e1b4b96fc2e16d30da802c54e5
HaydenLangill <84516925+Hayden-Langill@users.noreply.github.com>
2021-06-09 16:28:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let date = 0
+let age = 0
+
+document.getElementById('button').addEventListener('click', promo)
+
+function promo () {
+  age = document.getElementById('input1').value
+  age = parseInt(age)
+  date = document.getElementById('input2').value
+  if ((date == 'Monday' || date == 'Sunday') || (age > 2 && age < 10) || (age >= 60)) {
+    document.getElementById('output').innerHTML = 'You get 1 free pass to the museum!'
+  } else {
+    document.getElementById('output').innerHTML = 'You are not eligible for a free pass.'
+  }
+}
